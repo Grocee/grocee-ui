204981d6cdb3b4b290d2265136da0fdd1f556d4e
Indira Pranabudi <indirapranabudi@gmail.com>
2018-03-21 20:27:47 -0400

    Display checked status properly for grocery item, and also call the API when item is checked

--- src/Grocery/Grocery.js
+++ src/Grocery/Grocery.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
-
 import { CheckBox } from 'react-native-elements';
+
+import Meteor from 'react-native-meteor';
 
 export default class Grocery extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			checked: false
-		};
 	}
 
 	handlePress() {
-		this.setState(prevState => ({checked: !prevState.checked}));
+		let checked = !this.props.item.checked;
+		Meteor.call('groceries.setChecked', this.props.item._id, checked);
 	}
 
 	render() {
 		const item = this.props.item;
 		return (
-			<CheckBox title={`${item.amount} ${item.name}`} checkedColor='green' checked={this.state.checked} onPress={() => this.handlePress()} onIconPress={() => this.handlePress()}/>
+			<CheckBox 
+				title={`${item.amount} ${item.name}`}
+				checkedColor='green'
+				checked={item.checked}
+				onPress={() => this.handlePress()}
+				onIconPress={() => this.handlePress()}/>
 		);
 	}
 }
