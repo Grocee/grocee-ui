9d6c86cc4e5782b90d28de6a8e08c42db139d9c8
Zack Apiratitham <zack@vatthikorn.com>
2018-07-25 22:10:41 -0400

    Add default inventory list setting

---
+++ src/Settings/SelectInventoryList.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
+import Meteor, { Accounts } from 'react-native-meteor';
+import TableView from 'react-native-tableview'
+const { Section, Item } = TableView;
+
+export default class SelectInventoryList extends Component {
+
+	constructor(props) {
+		super(props);
+
+		const defaultLists = this.props.screenProps.inventoryLists.filter(list => list.isDefault);
+
+		let defaultList;
+		if (defaultLists.length < 1) {
+			defaultList = this.props.screenProps.inventoryLists[0];
+		} else {
+			defaultList = defaultLists[0];
+		}
+
+		this.state = {
+			lists: this.props.screenProps.inventoryLists,
+			defaultList
+		};
+
+	}
+
+	static navigationOptions({ navigation }) {
+		return {
+			headerTitle: 'Default Inventory List'
+		};
+	}
+
+	setDefaultList(item) {
+		const inventoryList = this.state.lists[item.selectedIndex];
+
+		// remove the old default list then set a new one
+		Meteor.call('inventorylists.setDefault', this.state.defaultList._id, false);
+		Meteor.call('inventorylists.setDefault', inventoryList._id, true);
+
+		this.props.navigation.goBack();
+	}
+
+	render() {
+
+		return (
+			<SafeAreaView style={StyleSheet.absoluteFill}>
+				<TableView
+					style={{ flex: 1 }}
+					tableViewStyle={TableView.Consts.Style.Grouped}
+					tableViewCellStyle={TableView.Consts.CellStyle.Default}>
+					<Section>
+						{this.state.lists.map(list =>
+							<Item
+								onPress={(item) => this.setDefaultList(item)}
+								key={list._id}
+								accessoryType={list.isDefault
+									? TableView.Consts.AccessoryType.Checkmark
+									: TableView.Consts.AccessoryType.None}>
+								{list.name}
+							</Item>
+						)}
+					</Section>
+				</TableView>
+			</SafeAreaView>
+		);
+	}
+}
